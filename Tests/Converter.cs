88680124345a34c285e6411e1035057367bba9eb
Simon Cropp <simon.cropp@gmail.com>
2018-02-12 14:11:12 +1100

    .

---
+++ Tests/Converter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using Mono.Cecil;
+using Xunit;
+
+public class Converter
+{
+    [Fact]
+    public void Run()
+    {
+        var binariesPath = GetBinariesPath();
+        var source = Path.Combine(binariesPath, "Source");
+        var converted = Path.Combine(binariesPath, "Converted");
+        ModuleDefinition.ReadModule()
+    }
+
+    string GetBinariesPath([System.Runtime.CompilerServices.CallerFilePath] string sourceFilePath = "")
+    {
+        return Path.GetFullPath(Path.Combine("../../Binaries", sourceFilePath));
+    }
+}
